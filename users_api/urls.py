acef199251a8841c1e8659791a29965f9c0acfe8
Leonardo Paz Estevam <leopestevam@hotmail.com>
2022-04-19 17:01:59 -0300

    Api para deletar usuarios

--- users_api/urls.py
+++ users_api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path 
-from .views import UserStaffListarCriarView
+from .views import UserStaffListarCriarView, UserStaffRecuperarDeletarView
 
 app_name = "users_api"
 
 urlpatterns = [
-    path("listar-criar-users/", UserStaffListarCriarView.as_view(), name="listar-criar-users")
+    #Staff
+    path("listar-criar-users/", UserStaffListarCriarView.as_view(), name="listar-criar-users"),
+    path("deletar-users/<int:pk>", UserStaffRecuperarDeletarView.as_view(), name="deletar-users")
 ]
--- users_api/views.py
+++ users_api/views.py
@@ -1,8 +1,14 @@
 from .serializers import UserSerializer
 from django.contrib.auth.models import User
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
 
+#Staff
 # api/users/listar-criar-users/
 class UserStaffListarCriarView(ListCreateAPIView): 
+    queryset = User.objects.all()
+    serializer_class = UserSerializer 
+
+# api/users/recuperar-deletar-users/
+class UserStaffRecuperarDeletarView(RetrieveDestroyAPIView): 
     queryset = User.objects.all()
     serializer_class = UserSerializer
