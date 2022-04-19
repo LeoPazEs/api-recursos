bc8a0eac7a148120b37a02c4b36bc0938c076732
Leonardo Paz Estevam <leopestevam@hotmail.com>
2022-04-19 17:31:54 -0300

    Retornando o usuario que alocou

--- recursos/serializers.py
+++ recursos/serializers.py
@@ -1,7 +1,15 @@
 from core.my_serializers import DynamicFieldsModelSerializer
-from .models import Recurso 
+from .models import Recurso  
+from rest_framework import serializers
 
 class RecursoSerializer(DynamicFieldsModelSerializer): 
+    user = serializers.StringRelatedField(read_only=True, default=serializers.CurrentUserDefault())
+
     class Meta: 
         model = Recurso 
-        fields = "__all__" 
+        fields = "__all__"  
+
+    def save(self, **kwargs):
+        """Include default for read_only `user` field"""
+        kwargs["user"] = self.fields["user"].get_default()
+        return super().save(**kwargs)
