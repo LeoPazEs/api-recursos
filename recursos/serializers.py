643ec2222fdabffbe8893f510e042afa385f1dbc
Leonardo Paz Estevam <leopestevam@hotmail.com>
2022-04-21 20:09:30 -0300

    Colocando o validar_periodo_alocacao como method do serializer (static)

--- recursos/serializers.py
+++ recursos/serializers.py
@@ -4,16 +4,18 @@ from rest_framework import serializers
 from django.utils import timezone
 from django.db.models import Q
 
-def validar_periodo_alocacao(recurso, data_alocacao, data_devolucao): 
-    if recurso.data_maxima_alocacao < data_alocacao: raise serializers.ValidationError({"data_alocacao" :"Data de alocação depois da data máxima."}) 
-    if recurso.periodo_maximo_alocacao and recurso.periodo_maximo_alocacao < (data_devolucao - data_alocacao).days :  raise serializers.ValidationError("Período de alocação maior que período máximo.")
-    if Alocacao.objects.filter(Q(recurso = recurso) & Q(data_devolucao__gte = timezone.localtime(timezone.now()).date()) & (Q(data_devolucao__gte =  data_alocacao) & Q(data_alocacao__lte = data_devolucao))).exists():
-        raise serializers.ValidationError("Período de alocação conflita com outro período.") 
-    return recurso
 
 class AlocacaoSerializer(DynamicFieldsModelSerializer): 
     alocador = serializers.HiddenField(default = serializers.CurrentUserDefault())  
     recurso = serializers.PrimaryKeyRelatedField(read_only = True) 
+
+    @staticmethod
+    def validar_periodo_alocacao(recurso, data_alocacao, data_devolucao): 
+        if recurso.data_maxima_alocacao < data_alocacao: raise serializers.ValidationError({"data_alocacao" :"Data de alocação depois da data máxima."}) 
+        if recurso.periodo_maximo_alocacao and recurso.periodo_maximo_alocacao < (data_devolucao - data_alocacao).days :  raise serializers.ValidationError("Período de alocação maior que período máximo.")
+        if Alocacao.objects.filter(Q(recurso = recurso) & Q(data_devolucao__gte = timezone.localtime(timezone.now()).date()) & (Q(data_devolucao__gte =  data_alocacao) & Q(data_alocacao__lte = data_devolucao))).exists():
+            raise serializers.ValidationError("Período de alocação conflita com outro período.") 
+        return recurso
 
     def validate(self, data):
         data_alocacao = data["data_alocacao"] 
@@ -24,7 +26,7 @@ class AlocacaoSerializer(DynamicFieldsModelSerializer):
         except Recurso.DoesNotExist: 
             raise serializers.ValidationError("Recurso inválido!")
 
-        data["recurso"] = validar_periodo_alocacao(recurso, data_alocacao, data_devolucao)
+        data["recurso"] = AlocacaoSerializer.validar_periodo_alocacao(recurso, data_alocacao, data_devolucao)
         return data
 
     class Meta: 
